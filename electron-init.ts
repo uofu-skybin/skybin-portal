5f4406d0f514a6322a3121099aa6fedbf4d752db
Alex Steele <steeleal123@gmail.com>
2017-11-20 13:15:40 -0700

    Increase window size.

--- electron-init.ts
+++ electron-init.ts
@@ -3,8 +3,8 @@ let win;
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
-    height: 500,
+    width: 1200,
+    height: 800,
     backgroundColor: '#ffffff',
     icon: `file://${__dirname}/dist/assets/logo.png`
   });
