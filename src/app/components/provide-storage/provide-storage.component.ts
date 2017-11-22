babeb67ae7723f728c085ddfd05600a7281ce9d9
tesla <zak.peters13@gmail.com>
2017-11-21 19:21:17 -0700

    VERY rough implementation of contract listing

--- src/app/components/provide-storage/provide-storage.component.ts
+++ src/app/components/provide-storage/provide-storage.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
+
+
+interface Contract {
+  storageSpace: string;
+  renterID: string;
+}
+
+interface ContractsResponse {
+  contracts: Contract[];
+}
 
 @Component({
   selector: 'app-provide-storage',
@@ -6,11 +18,25 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./provide-storage.component.css'],
   encapsulation: ViewEncapsulation.None
 })
+
+
 export class ProvideStorageComponent implements OnInit {
+      myContracts: Contract[] = [];
+    
+      constructor(private http: HttpClient) { }
+    
+      ngOnInit() {
+        this.loadContracts();
+      }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+      private loadContracts() {
+        this.http.get<ContractsResponse>('http://127.0.0.1:8003/contracts').subscribe(response => {
+            console.log(response);
+            if (response.contracts) {
+                response.contracts.forEach(contract => {
+                    this.myContracts.push(contract);
+                });
+            }
+        });
+    }
 }
