49c697e4e6ff9ba7f2ccae609234e34451687a98
Alex Steele <steeleal123@gmail.com>
2017-11-24 19:11:31 -0700

    Add my-wallet sample.

--- src/app/components/my-wallet/my-wallet.component.ts
+++ src/app/components/my-wallet/my-wallet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-my-wallet',
@@ -8,9 +9,36 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class MyWalletComponent implements OnInit {
 
+  private transactions = [];
+  private dataSource = null;
+  private displayedColumns = ['date', 'user', 'type', 'amount'];
+
   constructor() { }
 
   ngOnInit() {
+    this.generateTransactions();
+    this.dataSource = new MatTableDataSource(this.transactions);
+  }
+
+  generateTransactions() {
+    const users = [
+      'Kincaid', 'Gradey', 'Zak', 'Alex'
+    ];
+    const types = [
+      'payment',
+      'receipt',
+    ];
+    const transactions = [];
+    for (let i = 0; i < 10; i++) {
+      const date = new Date(2017, Math.floor(Math.random() * 11 + 1), Math.floor(Math.random() * 30 + 1), 0, 0, 0, 0);
+      transactions.push({
+        'date': date,
+        'user': users[Math.round(Math.random() * (users.length - 1))],
+        'type': types[Math.round(Math.random() * (types.length - 1))],
+        'amount': Math.round(Math.random() * 100 * 100) / 100,
+      });
+    }
+    this.transactions = transactions;
   }
 
 }
