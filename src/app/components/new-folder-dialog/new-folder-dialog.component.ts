d7aa179b199e67a5173bad9fc9578442d33a59cf
Alex Steele <steeleal123@gmail.com>
2017-11-30 19:07:12 -0700

    Update Create Folder dialog.
    - Add cancel button.
    - Change placeholder to "New Folder".
    - Reposition buttons.

--- src/app/components/new-folder-dialog/new-folder-dialog.component.ts
+++ src/app/components/new-folder-dialog/new-folder-dialog.component.ts
@@ -1,22 +1,17 @@
 import { Component, OnInit, ViewEncapsulation, Inject } from '@angular/core';
 import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 
-
 @Component({
     selector: 'app-new-folder-dialog',
     templateUrl: './new-folder-dialog.component.html',
     styleUrls: ['./new-folder-dialog.component.css'],
     encapsulation: ViewEncapsulation.None
 })
-export class NewFolderDialogComponent implements OnInit {
+export class NewFolderDialogComponent {
 
-    constructor(
-        public dialogRef: MatDialogRef<NewFolderDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) public data: any) { }
+    name = '';
 
-    name: string = '';
-
-    ngOnInit() {
+    constructor(public dialogRef: MatDialogRef<NewFolderDialogComponent>) {
 
     }
 
