f073c883f0c8fba4ce022336ce28221ed875a87a
Kincaid Savoie <kincaidsavoie@gmail.com>
2017-11-30 19:58:40 -0700

    Fixed wallet tab up a bit.

--- src/app/components/my-wallet/my-wallet.component.ts
+++ src/app/components/my-wallet/my-wallet.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
 
+interface Wallet {
+    name: string;
+    id: string;
+    funds: number;
+    spent: number;
+    received: number;
+}
+
 @Component({
     selector: 'app-my-wallet',
     templateUrl: './my-wallet.component.html',
@@ -12,6 +20,33 @@ export class MyWalletComponent implements OnInit {
     private transactions = [];
     private dataSource = null;
     private displayedColumns = ['date', 'user', 'type', 'amount'];
+
+    private wallets = [
+        {
+            name: 'Rent wallet',
+            id: '7834-4327-3432',
+            funds: 50.00,
+            spent: 24.02,
+            received: 0
+        },
+        {
+            name: 'Provide wallet',
+            id: '4324-7657-4343',
+            funds: 124.02,
+            spent: 0,
+            received: 64.02
+        },
+    ];
+
+    private empty = {
+        name: '',
+        id: '',
+        funds: 0,
+        spent: 0,
+        received: 0
+    };
+
+    private selected = this.empty;
 
     constructor() { }
 
@@ -29,9 +64,10 @@ export class MyWalletComponent implements OnInit {
             'receipt',
         ];
         const transactions = [];
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 25; i++) {
             const date = new Date(2017, Math.floor(Math.random() * 11 + 1), Math.floor(Math.random() * 30 + 1), 0, 0, 0, 0);
             transactions.push({
+                'wallet': this.wallets[Math.round(Math.random() * (this.wallets.length - 1))],
                 'date': date,
                 'user': users[Math.round(Math.random() * (users.length - 1))],
                 'type': types[Math.round(Math.random() * (types.length - 1))],
@@ -41,4 +77,11 @@ export class MyWalletComponent implements OnInit {
         this.transactions = transactions;
     }
 
+    selectWallet(wallet) {
+        this.selected = wallet;
+    }
+
+    formatDate(date) {
+        return date.toLocaleDateString().replace(/\//g, '-');
+    }
 }
