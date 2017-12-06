9cf0c6a367875ec6994e3807104e4cb3b56cc386
Gradey Cullins <u0866499@utah.edu>
2017-12-05 22:50:17 -0700

    Fixing my scrolling issues. Not at 100% but should be usable for the time being.

--- src/app/components/shared-with-me/shared-with-me.component.ts
+++ src/app/components/shared-with-me/shared-with-me.component.ts
@@ -22,9 +22,16 @@ export class SharedWithMeComponent implements OnInit {
         private ref: ChangeDetectorRef) {
         const file1 = new SkyFile(null, null, '1', false, '11-29-2017', 'file 1', 30);
         const file2 = new SkyFile(null, null, '2', false, '01-02-2015', 'file 2', 1);
-        const file3 = new SkyFile(null, null, '1', false, '04-13-2020', 'file 3', 15);
+        const file3= new SkyFile(null, null, '1', false, '04-13-2020', 'file 3', 15);
+        const file4 = new SkyFile(null, null, '1', false, '04-13-2020', 'file 3', 15);
+        const file5 = new SkyFile(null, null, '1', false, '04-13-2020', 'file 3', 15);
+        const file6 = new SkyFile(null, null, '1', false, '04-13-2020', 'file 3', 15);
+        const file7 = new SkyFile(null, null, '1', false, '04-13-2020', 'file 3', 15);
+        const file8 = new SkyFile(null, null, '1', false, '04-13-2020', 'file 3', 15);
+        const file9 = new SkyFile(null, null, '1', false, '04-13-2020', 'file 3', 15);
+        const file10 = new SkyFile(null, null, '1', false, '04-13-2020', 'file 3', 15);
         const dir1 = new SkyFile(null, null, '1', false, '08-08-2008', 'dir 1', 256);
-        this.myFiles = [file1, file2, file3, dir1];
+        this.myFiles = [file1, file2, file3, file4, file5, file6, file7, file8, file9, file10, dir1];
     }
 
     ngOnInit() {
