fedd0fec3c20cde4e55227ac1742a75750ca60d7
Alex Steele <steeleal123@gmail.com>
2017-12-07 16:54:36 -0700

    File details formatting fixes.

--- src/app/components/view-file-details/view-file-details.component.ts
+++ src/app/components/view-file-details/view-file-details.component.ts
@@ -1,43 +1,41 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 
 @Component({
-  selector: 'app-view-file-details',
-  templateUrl: './view-file-details.component.html',
-  styleUrls: ['./view-file-details.component.css']
+    selector: 'app-view-file-details',
+    templateUrl: './view-file-details.component.html',
+    styleUrls: ['./view-file-details.component.css']
 })
 export class ViewFileDetailsComponent implements OnInit {
 
-  file = null;
+    file = null;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { 
-    this.file = data.file;
-    console.log(this.file.blocks[0]);
-  }
-
-  ngOnInit() {
-  }
-
-  baseName(fileName: string) {
-    const pathElems = fileName.split('/');
-    return pathElems[pathElems.length - 1];
-  }
-
-  formatModTime(modString) {
-    const date = new Date(modString);
-    return date.toLocaleDateString().replace(/\//g, '-');
-  }
-
-  getFileLocations() {
-    const locations = [];
-    for (let block of this.file.blocks) {
-      for (let location of block.locations) {
-        if (locations.indexOf(location.address) === -1) {
-          locations.push(location.address)          
-        }
-      }
+    constructor( @Inject(MAT_DIALOG_DATA) public data: any) {
+        this.file = data.file;
     }
-    return locations;
-  }
+
+    ngOnInit() {
+    }
+
+    baseName(fileName: string) {
+        const pathElems = fileName.split('/');
+        return pathElems[pathElems.length - 1];
+    }
+
+    formatModTime(modString) {
+        const date = new Date(modString);
+        return date.toLocaleDateString().replace(/\//g, '-');
+    }
+
+    getFileLocations() {
+        const locations = this.file.blocks
+            .map(e => e.locations)
+
+            // Flatten the location lists.
+            .reduce((acc, e) => acc.concat(e), []);
+
+        const addrs = locations.map(e => e.address);
+        return addrs;
+    }
 }
