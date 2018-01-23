1ff93e86d3624c6ba9501659a20d21df4e5dcaad
Gradey Cullins <gradeycullins@gmail.com>
2018-01-22 23:22:49 -0700

    Added more IPC in electron land. Asynchronous routing is still causing issues.

--- src/app/modules/routing/routing.module.ts
+++ src/app/modules/routing/routing.module.ts
@@ -11,7 +11,6 @@ import { MyWalletComponent } from '../../components/my-wallet/my-wallet.componen
 const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     // { path: '', redirectTo: 'my-files', pathMatch: 'full' },
-    { path: 'my-files', redirectTo: 'my-files', pathMatch: 'full' },
     { path: 'login', pathMatch: 'full', component: LoginComponent },
     { path: 'register', pathMatch: 'full', component: RegisterComponent },
     { path: 'my-files', pathMatch: 'full', component: MyFilesComponent },
