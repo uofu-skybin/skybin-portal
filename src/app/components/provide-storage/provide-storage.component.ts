ee5486af51a6273245ff25e7c24a4f523a09ff51
Alex Steele <steeleal123@gmail.com>
2018-02-23 13:05:43 -0700

    Rough provider dashboard concept.

--- src/app/components/provide-storage/provide-storage.component.ts
+++ src/app/components/provide-storage/provide-storage.component.ts
@@ -1,14 +1,21 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MatSort, MatTableDataSource } from '@angular/material';
 import { HttpErrorResponse } from '@angular/common/http/src/response';
 import Timer = NodeJS.Timer;
 import { appConfig } from '../../models/config';
-import {Activity, ActivityResponse, Contract, ContractsResponse, InfoResponse} from '../../models/common';
-
+import { Activity, ActivityResponse, Contract, ContractsResponse, ProviderInfo } from '../../models/common';
+import * as d3 from 'd3';
+import * as Rickshaw from 'rickshaw';
+import * as Chart from 'chart.js';
 
 // Activity feed update interval (ms)
 const ACTIVITY_INTERVAL = 5 * 1000;
+
+console.log('got rickshaw!');
+console.log('rickshaw:', Rickshaw);
+console.log('d3:', d3);
+console.log('chartjs', Chart);
 
 @Component({
     selector: 'app-provide-storage',
@@ -19,8 +26,7 @@ const ACTIVITY_INTERVAL = 5 * 1000;
 export class ProvideStorageComponent implements OnInit, OnDestroy, AfterViewInit {
     private myContracts: Contract[] = [];
 
-    // TODO convert to structure as opposed to any object
-    providerInfo: InfoResponse = {};
+    providerInfo: ProviderInfo = {};
     activityFeed: Activity[] = [];
     displayedColumns = ['Request Type', 'Block ID', 'Renter ID', 'Timestamp'];
     dataSource = new MatTableDataSource<Activity>();
@@ -34,16 +40,118 @@ export class ProvideStorageComponent implements OnInit, OnDestroy, AfterViewInit
     ];
     @ViewChild(MatSort) sort: MatSort;
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient,
+        private ref: ChangeDetectorRef) {
     }
 
     ngOnInit() {
-        this.loadProviderInfo();
-        this.loadContracts();
-        // this.loadTestActivityData();
-        this.loadActivity();
-        console.log('starting poll service...');
-        this.activityPollId = setInterval(() => this.loadActivity(), ACTIVITY_INTERVAL);
+        // this.loadProviderInfo();
+        // this.loadContracts();
+        // // this.loadTestActivityData();
+        // this.loadActivity();
+        // console.log('starting poll service...');
+        // this.activityPollId = setInterval(() => this.loadActivity(), ACTIVITY_INTERVAL);
+
+        this.drawChart();
+        this.drawStorageUsed();
+    }
+
+    drawStorageUsed() {
+        let ctxt = document.getElementById('storagePieChart');
+        let chart = new Chart(ctxt, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    data: [10000, 7500],
+                    backgroundColor: [
+                        'red',
+                        'blue'
+                    ],
+                }],
+                labels: [
+                    'Allocated',
+                    'Used',
+                ],
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Storage Used',
+                },
+            },
+        });
+    }
+
+    drawChart() {
+        // let graph = new Rickshaw.Graph({
+        //     element: document.querySelector('#chart'),
+        //     width: 500,
+        //     height: 300,
+        //     series: [{
+        //         color: 'steelblue',
+        //         data: [
+        //             { x: 1, y: 10 },
+        //             { x: 2, y: 20 },
+        //             { x: 3, y: 30 },
+        //             { x: 4, y: 40 },
+        //             { x: 5, y: 50 },
+        //         ]
+        //     }]
+        // });
+
+        // let xAxis = new Rickshaw.Graph.Axis.X({
+        //     graph: graph,
+        // });
+
+        // let yAxis = new Rickshaw.Graph.Axis.Y({
+        //     graph: graph,
+        //     // element: document.getElementById('y-axis'),
+        // });
+
+        // let hoverDetail = new Rickshaw.Graph.HoverDetail({
+        //     graph: graph,
+        //     xFormatter: (x) => x + 'something',
+        //     yFormatter: (y) => y + 'value',
+        // });
+
+        // graph.render();
+
+        let ctxt = (<any>document.getElementById('myChart')).getContext('2d');
+        let myChart = new Chart(ctxt, {
+            type: 'line',
+            data: {
+                // label: 'Storage Contracts over Time',
+                datasets: [
+                    {
+                        labels: ['a', 'b', 'c', 'd', 'e'],
+                        data: [3, 4, 5, 6, 7, 3, 4, 5, 7, 12],
+                    }
+                ],
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Storage Use Over Time',
+                },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Date',
+                        },
+                    }],
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Total Contracts',
+                        },
+                    }],
+                },
+                legend: {
+                    display: false,
+                },
+            },
+        });
     }
 
     // Necessary for the mat-table column sorting.
