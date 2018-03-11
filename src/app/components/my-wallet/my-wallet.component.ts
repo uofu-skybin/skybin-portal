f6997895347905b0414c6f6ebd724466a9a7e0a4
Kincaid Savoie <kincaid@hoodoo.digital>
2018-03-10 21:23:59 -0700

    got paypal button to show up

--- src/app/components/my-wallet/my-wallet.component.ts
+++ src/app/components/my-wallet/my-wallet.component.ts
@@ -1,13 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
-
-interface Wallet {
-    name: string;
-    id: string;
-    funds: number;
-    spent: number;
-    received: number;
-}
+import paypal = require('paypal-checkout');
 
 @Component({
     selector: 'app-my-wallet',
@@ -16,72 +9,56 @@ interface Wallet {
     encapsulation: ViewEncapsulation.None
 })
 export class MyWalletComponent implements OnInit {
-
-    private transactions = [];
-    private dataSource = null;
-    private displayedColumns = ['date', 'user', 'type', 'amount'];
-
-    private wallets = [
-        {
-            name: 'Rent wallet',
-            id: '7834-4327-3432',
-            funds: 50.00,
-            spent: 24.02,
-            received: 0
-        },
-        {
-            name: 'Provide wallet',
-            id: '4324-7657-4343',
-            funds: 124.02,
-            spent: 0,
-            received: 64.02
-        },
-    ];
-
-    private empty = {
-        name: '',
-        id: '',
-        funds: 0,
-        spent: 0,
-        received: 0
-    };
-
-    private selected = this.empty;
+    @ViewChild('paypalButton') paypalButton: ElementRef;
 
     constructor() { }
 
     ngOnInit() {
-        this.generateTransactions();
-        this.dataSource = new MatTableDataSource(this.transactions);
+        
     }
 
-    generateTransactions() {
-        const users = [
-            'Kincaid', 'Gradey', 'Zak', 'Alex'
-        ];
-        const types = [
-            'payment',
-            'receipt',
-        ];
-        const transactions = [];
-        for (let i = 0; i < 25; i++) {
-            const date = new Date(2017, Math.floor(Math.random() * 11 + 1), Math.floor(Math.random() * 30 + 1), 0, 0, 0, 0);
-            transactions.push({
-                'wallet': this.wallets[Math.round(Math.random() * (this.wallets.length - 1))],
-                'date': date,
-                'user': users[Math.round(Math.random() * (users.length - 1))],
-                'type': types[Math.round(Math.random() * (types.length - 1))],
-                'amount': Math.round(Math.random() * 100 * 100) / 100,
-            });
-        }
-        this.transactions = transactions;
-    }
-
-    selectWallet(wallet) {
-        this.selected = wallet;
-    }
-
-    formatDate(date) {
-        return date.toLocaleDateString().replace(/\//g, '-');
+    ngAfterViewInit() {
+        paypal.Button.render({
+            payment: function() {
+                return new paypal.Promise(function(resolve, reject) {
+    
+                    // Make an ajax call to get the Payment ID. This should call your back-end,
+                    // which should invoke the PayPal Payment Create api to retrieve the Payment ID.
+    
+                    // When you have a Payment ID, you need to call the `resolve` method, e.g `resolve(data.paymentID)`
+                    // Or, if you have an error from your server side, you need to call `reject`, e.g. `reject(err)`
+    
+                    // $.post('/my-api/create-payment')
+                        // .done(function(data) { resolve(data.paymentID); })
+                        // .fail(function(err)  { reject(err); });
+                });
+            },
+    
+            // Pass a function to be called when the customer approves the payment,
+            // then call execute payment on your server:
+    
+            onAuthorize: function(data) {
+    
+                console.log('The payment was authorized!');
+                console.log('Payment ID = ',   data.paymentID);
+                console.log('PayerID = ', data.payerID);
+    
+                // At this point, the payment has been authorized, and you will need to call your back-end to complete the
+                // payment. Your back-end should invoke the PayPal Payment Execute api to finalize the transaction.
+    
+                // jQuery.post('/my-api/execute-payment', { paymentID: data.paymentID, payerID: data.payerID })
+                //     .done(function(data) { /* Go to a success page */ })
+                //     .fail(function(err)  { /* Go to an error page  */  });
+            },
+    
+            // Pass a function to be called when the customer cancels the payment
+    
+            onCancel: function(data) {
+    
+                console.log('The payment was cancelled!');
+                console.log('Payment ID = ', data.paymentID);
+            }
+    
+        }, this.paypalButton.nativeElement);
     }
 }
