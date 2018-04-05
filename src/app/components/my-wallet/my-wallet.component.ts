7003c2735bfee67c1e4165e71090b6c431d72a97
Kincaid Savoie <kincaid@hoodoo.digital>
2018-04-04 18:18:57 -0600

    more gross hacks for the paypal button. ¯\_(ツ)_/¯

--- src/app/components/my-wallet/my-wallet.component.ts
+++ src/app/components/my-wallet/my-wallet.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
-import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatTabChangeEvent } from '@angular/material';
 import { Router } from '@angular/router';
 import {appConfig} from '../../models/config';
 import { RenterInfo, ProviderInfo, Transaction, TransactionsResponse } from '../../models/common';
@@ -61,41 +61,7 @@ export class MyWalletComponent implements OnInit {
     }
 
     ngAfterViewInit() {
-        paypal.Button.render({
-            env: 'sandbox',
-            payment: () => {
-                this.electronService.ipcRenderer.send('close-paypal', '');
-                // HACK! This gets the path to the file by swapping out the route with the file name.
-                let url = window.location.href.replace('my-wallet', 'index.html');
-                return paypal.request.post(
-                    `${appConfig['renterAddress']}/paypal/create`,
-                    {
-                        'amount': this.depositAmount * 100,
-                        'returnURL': url,
-                        'cancelURL': url,
-                    }
-                ).then(function(data) { return data.id; });
-            },
-    
-            onAuthorize: (data) => {
-                return paypal.request.post(`${appConfig['renterAddress']}/paypal/execute`,
-                {
-                    paymentID: data.paymentID,
-                    payerID: data.payerID,
-                }).then(() => {
-                    this.updateRenterBalance();
-                    this.showNotification("Deposit successful!");
-                    this.depositAmount = null;
-                })
-            },
-    
-            // Pass a function to be called when the customer cancels the payment
-    
-            onCancel: (data) => {
-                this.showNotification('Payment canceled!');
-            }
-    
-        }, this.paypalButton.nativeElement);
+        this.renderPaypal();
 
         this.dataSource.paginator = this.paginator;
     }
@@ -201,5 +167,54 @@ export class MyWalletComponent implements OnInit {
     getDate(time: string) {
         let date =  new Date(time);
         return date.toString();
+    }
+
+    onTabSwitched(event: MatTabChangeEvent) {
+        // Stupid gross hack. The paypal button blows up if we switch to another tab. So, we remove it when we switch
+        // away from the home tab, and rerender it when we switch back.
+        if (event.index == 0) {
+            this.renderPaypal();
+        } else {
+            this.paypalButton.nativeElement.innerHTML = '';
+        }
+    }
+
+    renderPaypal() {
+        console.log('rendering paypal butotn');
+        paypal.Button.render({
+            env: 'sandbox',
+            payment: () => {
+                this.electronService.ipcRenderer.send('close-paypal', '');
+                // HACK! This gets the path to the file by swapping out the route with the file name.
+                let url = window.location.href.replace('my-wallet', 'index.html');
+                return paypal.request.post(
+                    `${appConfig['renterAddress']}/paypal/create`,
+                    {
+                        'amount': this.depositAmount * 100,
+                        'returnURL': url,
+                        'cancelURL': url,
+                    }
+                ).then(function(data) { return data.id; });
+            },
+    
+            onAuthorize: (data) => {
+                return paypal.request.post(`${appConfig['renterAddress']}/paypal/execute`,
+                {
+                    paymentID: data.paymentID,
+                    payerID: data.payerID,
+                }).then(() => {
+                    this.updateRenterBalance();
+                    this.showNotification("Deposit successful!");
+                    this.depositAmount = null;
+                })
+            },
+    
+            // Pass a function to be called when the customer cancels the payment
+    
+            onCancel: (data) => {
+                this.showNotification('Payment canceled!');
+            }
+    
+        }, this.paypalButton.nativeElement);
     }
 }
